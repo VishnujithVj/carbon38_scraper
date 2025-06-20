141ff91ad33463b17a1c2de81be92e63f173d379
VishnujithVj <vishnuvj801@gmail.com>
2025-06-20 18:41:18 +0000

    Defined structured item fields for product data in items.py

--- carbon38_scraper/items.py
+++ carbon38_scraper/items.py
@@ -7,6 +7,18 @@ import scrapy
 
 
 class Carbon38ScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    
+    breadcrumbs = scrapy.Field()
+    primary_image_url = scrapy.Field()
+    brand = scrapy.Field()
+    product_name = scrapy.Field()
+    price = scrapy.Field()
+    reviews = scrapy.Field()
+    colour = scrapy.Field()
+    sizes = scrapy.Field()
+    description = scrapy.Field()
+    sku = scrapy.Field()
+    product_id = scrapy.Field()
+    product_url = scrapy.Field()
+    image_urls = scrapy.Field()
     pass
